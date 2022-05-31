7646c5e3b156371bd5a25aa8ec6a73e77e4a603c
Wamaitha <wamaithaweru19@gmail.com>
2022-05-31 14:14:56 +0300

    deleted Imagegallery model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
@@ -5,13 +6,17 @@ class Location(models.Model):
     venue = models.CharField(null=True, blank=True, max_length=100)
     id = models.CharField(max_length=50,primary_key=True, unique=True, blank=True)
     address=models.CharField(null=True, blank=True, max_length=100)
-    date_created = models.DateTimeField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True,blank=True, null=True)
 
-    def __str__(self):
-        return self.venue
 
     def save_Location(self):
         self.save()
+    
+    def delete_location(self): 
+        self.delete() 
+    
+    def __str__(self):
+        return self.venue
 
 
 class Category(models.Model):
@@ -21,10 +26,6 @@ class Category(models.Model):
     post_date = models.DateTimeField(auto_now_add=True)
     
     
-    
-    def __str__(self):
-        return self.title
-
     def save_category(self):
         self.save()
 
@@ -34,25 +35,36 @@ class Category(models.Model):
         category = cls.objects.filter(title__icontains=searched_term)
         return category
     
+    def __str__(self):
+        return self.title
 
-class Imagegallery(models.Model):
+
+
+
+class Pictures(models.Model):
     name = models.CharField(max_length=50)
     id = models.CharField(max_length=50,primary_key=True, unique=True, blank=True)
     description = models.TextField(max_length=100)
-    date_created = models.DateTimeField(blank=True, null=True)
-    image = models.ImageField(upload_to='images/')
-    web_address=models.FilePathField(null=True, blank=True, max_length=100,unique=True)
+    date_created = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    image = models.ImageField(upload_to='pics/')
     hashtags = models.CharField(null=True, blank=True, max_length=100)
     location = models.ForeignKey(Location,null=True, blank=True,on_delete=models.CASCADE)
     category = models.ForeignKey(Category,null=True, blank=True,on_delete=models.CASCADE)
     
-
-    def __str__(self):
-        return self.name
-
     #  save
     def save_imagegallery(self):
         self.save()
+     
+    @classmethod
+    def all_images(cls): 
+        images= Pictures.objects.all() 
+        return images
+    
+    @classmethod
+    def update_image(cls,current_img,new_img):
+        updated_img = Pictures.objects.filter(image_name=current_img).update(name=new_img)
+        return updated_img
 
-
-
+    def __str__(self):
+        return self.name
+        
